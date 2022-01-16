84822819093911da5047f9f1e2f2b6bc281829a1
YuChing1204 <69633328+YuChing1204@users.noreply.github.com>
2022-01-16 04:52:16 -0600

    connect mongoose

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
-let port = process.env.PORT || 3030;
+const todoRoutes = require("./routes/todoRoutes");
+let PORT = process.env.PORT || 3030;
+
+mongoose
+    .connect(
+        "mongodb+srv://YuChing:811204@todolist.yublz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+.then(() => {
+    console.log("Connect to mongodb")
+})
+.catch((err) => {
+    console.log(err);
+});
+
+app.use("/todos", todoRoutes);
 
 app.listen(PORT, () => {
     console.log("The server is listening on port" + PORT);
