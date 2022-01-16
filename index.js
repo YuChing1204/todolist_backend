cc67834e456d630fb744896282fb0942e0008040
YuChing1204 <69633328+YuChing1204@users.noreply.github.com>
2022-01-16 04:36:07 -0600

    v1.2

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require("express");
 const app = express();
 let port = process.env.PORT || 3030;
 
-app.get("/", (req, res) => {
-    res.send("Hello world")
-});
-
-app.listen(port, () => {
-    console.log("listening");
+app.listen(PORT, () => {
+    console.log("The server is listening on port" + PORT);
 });
